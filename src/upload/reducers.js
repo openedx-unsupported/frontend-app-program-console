b2fd6dfb3b92c02b0225c98331b4933a6cda5cd5
Rick Reilly <rreilly@edx.org>
2019-07-09 16:06:09 -0400

    Program Manager with Upload and Downloads and banners for all of the validations there in

--- src/upload/reducers.js
+++ src/upload/reducers.js
@@ -1,13 +1,16 @@
-import { FETCH_WRITABLE_PROGRAMS } from './actions';
+import { FETCH_WRITABLE_PROGRAMS, UPLOAD_ENROLLMENTS, DOWNLOAD_ENROLLMENTS, POLL_JOB, FETCH_JOBS } from './actions';
 
 export const defaultState = {
   loading: false,
   loaded: false,
   loadingError: null,
+  authorized: true,
   data: [],
+  programBanners: {},
 };
 
 const example = (state = defaultState, action) => {
+  const { programBanners } = state;
   switch (action.type) {
     case FETCH_WRITABLE_PROGRAMS.BEGIN:
       return {
@@ -19,14 +22,20 @@ const example = (state = defaultState, action) => {
     case FETCH_WRITABLE_PROGRAMS.SUCCESS:
       return {
         ...state,
+        authorized: true,
         data: action.payload.data,
-        loading: false,
-        loaded: true,
+        programBanners: action.payload.data.reduce((acc, curVal) => {
+          acc[curVal.programKey] = [];
+          return acc;
+        }, {}),
+        loading: true,
+        loaded: false,
         loadingError: null,
       };
     case FETCH_WRITABLE_PROGRAMS.FAILURE:
       return {
         ...state,
+        authorized: false,
         loading: false,
         loaded: false,
         loadingError: action.payload.error,
@@ -37,6 +46,114 @@ const example = (state = defaultState, action) => {
         loading: false,
         loaded: false,
         loadingError: null,
+      };
+    case FETCH_JOBS.BEGIN:
+      return {
+        ...state,
+        loading: true,
+        loaded: false,
+      };
+    case FETCH_JOBS.SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+      };
+    case FETCH_JOBS.FAILURE:
+      return {
+        ...state,
+        loading: false,
+        loaded: false,
+        loadingError: action.payload.error,
+      };
+    case FETCH_JOBS.RESET:
+      return {
+        ...state,
+        loading: false,
+        loaded: false,
+        loadingError: null,
+      };
+    case 'ADD_BANNER':
+      return {
+        ...state,
+        programBanners: {
+          ...programBanners,
+          [action.payload.programKey]: [
+            ...programBanners[action.payload.programKey],
+            action.payload.bannerObj,
+          ],
+        },
+      };
+    case 'REMOVE_BANNER':
+      return {
+        ...state,
+        programBanners: {
+          ...programBanners,
+          [action.payload.programKey]:
+            programBanners[action.payload.programKey]
+              .filter(banner => banner.id !== action.payload.bannerId),
+        },
+      };
+    case UPLOAD_ENROLLMENTS.SUCCESS:
+      return {
+        ...state,
+        programBanners: {
+          ...programBanners,
+          [action.payload.programKey]: [
+            ...programBanners[action.payload.programKey],
+            action.payload.bannerObj,
+          ],
+        },
+      };
+    case UPLOAD_ENROLLMENTS.FAILURE:
+      return {
+        ...state,
+        programBanners: {
+          ...programBanners,
+          [action.payload.programKey]: [
+            ...programBanners[action.payload.programKey],
+            action.payload.bannerObj,
+          ],
+        },
+      };
+    case DOWNLOAD_ENROLLMENTS.SUCCESS:
+      return {
+        ...state,
+        programBanners: {
+          ...programBanners,
+          [action.payload.programKey]: [
+            ...programBanners[action.payload.programKey],
+            action.payload.bannerObj,
+          ],
+        },
+      };
+    case DOWNLOAD_ENROLLMENTS.FAILURE:
+      return {
+        ...state,
+        programBanners: {
+          ...programBanners,
+          [action.payload.programKey]: [
+            ...programBanners[action.payload.programKey],
+            action.payload.bannerObj,
+          ],
+        },
+      };
+
+    case 'NOT_AUTHENTICATED':
+      return {
+        ...state,
+        authorized: false,
+      };
+    case POLL_JOB.SUCCESS:
+      return {
+        ...state,
+        programBanners: {
+          ...programBanners,
+          [action.payload.programKey]: [
+            ...programBanners[action.payload.programKey],
+            action.payload.bannerObj,
+          ],
+        },
       };
     default:
       return state;
