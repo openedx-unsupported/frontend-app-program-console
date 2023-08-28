d54e1678bd0f94f72272130b164ef60919761bb1
Syed Ali Abbas Zaidi <syed.ali.abbas@arbisoft.com>
2023-08-28 17:32:28 +0500

    refactor: remove connected-react-router

--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux';
-import { connectRouter } from 'connected-react-router';
 import {
   reducer as consoleReducer,
   storeName as consoleStoreName,
@@ -10,10 +9,9 @@ import {
   storeName as reportStoreName,
 } from './report';
 
-const createRootReducer = history => combineReducers({
+const createRootReducer = () => combineReducers({
   [consoleStoreName]: consoleReducer,
   [reportStoreName]: reportReducer,
-  router: connectRouter(history),
 });
 
 export default createRootReducer;
